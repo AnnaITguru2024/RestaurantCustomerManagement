617bbfea14b7e9ced895aa47543fdfadb46ad53e
AnnaITguru2024 <annetteshilova90@gmail.com>
2024-12-04 12:56:12 +0900

    初回コミット

---
+++ src/test/java/portfolio/RestaurantCustomerManagement/RestaurantCustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.RestaurantCustomerManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
